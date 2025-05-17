93d7a0f76631b484028975009eb4a44390352468
thuphanIBM <thuphan@us.ibm.com>
2025-05-16 17:18:50 -0700

    Update db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE properties (
+  case_type VARCHAR(25),
+  symbolicName VARCHAR(25),
+  value VARCHAR(100),
+  customValidationError VARCHAR(255),
+  customInvalidItems CHAR[],
+  displayMode VARCHAR(10),
+  updatability VARCHAR(10),
+  required VARCHAR(25),
+  hidden BOOLEAN,
+  hasDependentProperties BOOLEAN,
+  choiceList VARCHAR(25)
+);
